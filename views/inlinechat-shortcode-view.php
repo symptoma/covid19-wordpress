806e34bbb55a0c8afbaa74edf7feacff27392fc8
ny laptop <helmuth.lammer@gmail.com>
2020-07-28 11:09:39 +0200

    changed domain in shortcode view to .net for international usage

--- views/inlinechat-shortcode-view.php
+++ views/inlinechat-shortcode-view.php
@@ -2,4 +2,4 @@
     var symptoma_mode = 'CORONA';
     var symptoma_integration = 'inline';
 </script>
-<script id="symptoma-integration" type="text/javascript" src="https://www.symptoma.at/de/embed.js"></script>
+<script id="symptoma-integration" type="text/javascript" src="https://www.symptoma.net/de/embed.js"></script>
